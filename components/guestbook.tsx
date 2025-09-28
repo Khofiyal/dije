4a644cde7910c5905d8e3cdbaa425004d8b3922a
Fatah <khofiyalfatah@gmail.com>
2025-09-28 08:23:17 +0700

    changes

--- components/guestbook.tsx
+++ components/guestbook.tsx
@@ -5,160 +5,179 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
-import { Toaster } from './ui/sonner';
+import { Toaster } from "./ui/sonner";
 import { toast } from "sonner";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
-
 function Guestbook() {
-    const [isCreated, setIsCreated] = useState(false)
-    const [name, setName] = useState("");
-    const [attendees, setAttendees] = useState("");
-    const [guest, setGuest] = useState("");
-    const [message, setMessage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false); // <-- state loading
+  const [isCreated, setIsCreated] = useState(false);
+  const [name, setName] = useState("");
+  const [attendees, setAttendees] = useState("");
+  const [guest, setGuest] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        const data = {
-            name,
-            attendees,
-            guest,
-            message,
-        };
-
-        if (!name || !attendees || !message) {
-            toast.error("Please fill in all fields");
-            return;
-        }
-
-        try {
-            await fetch("/api/guestbook", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            });
-
-            setIsCreated(true);
-            toast.success("Success");;
-        } catch (error) {
-            console.error(error);
-        }
+    const data = {
+      name,
+      attendees,
+      guest,
+      message,
     };
 
-    return (
-        <div className="font-regular items-center mx-auto">
-            <Card className="flex flex-col md:w-150 lg:w-[700px] w-85 bg-[#F9F5EC]  text-black lg:px-14 px-5 mx-auto lg:py-8 py-5">
-                <CardHeader>
-                    <CardTitle className="lg:text-3xl text-xl uppercase text-[#9D8443] text-center">attendance</CardTitle>
-                </CardHeader>
-                <CardContent className="">
-                    <form onSubmit={handleSubmit} className="flex flex-col gap-7">
-                        <div className="flex flex-col gap-1">
-                            <Label className="text-md" htmlFor="name">Guest Name</Label>
-                            <Input
-                                className=""
-                                type="text"
-                                id="name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <Label className="text-md" htmlFor="email">Attendance</Label>
-                            <div className="w-full grid grid-cols-2 gap-2 text-center items-center">
-                                <label className="cursor-pointer">
-                                    <input
-                                    type="radio"
-                                    className="peer sr-only"
-                                    id="attendees"
-                                    name="attendees" 
-                                    value="attending"
-                                    onChange={(e) => setAttendees(e.target.value)}
-                                    />
-                                    <div className="lg:w-72 max-w-xl p-2 lg:py-2 py-3 rounded-md bg-[#F8EFD3] ring-2 ring-transparent border border-black/20 transition-all hover:shadow peer-checked:bg-[#F5DB8C]/75 peer-checked:ring-[#9D8443]/50 peer-checked:ring-offset-1 peer-checked:border-none">
-                                    <div className="flex flex-col gap-1">
-                                        <div className="items-center">
-                                        <p className="text-center lg:text-[16px] text-sm"> Will attend</p>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </label>
-                                <label className="cursor-pointer">
-                                    <input
-                                    type="radio"
-                                    className="peer sr-only"
-                                    name="attendees"
-                                    value="unable to attend"
-                                    onChange={(e) => setAttendees(e.target.value)}
-                                    />
-                                    <div className="lg:w-72 max-w-xl p- lg:py-2 py-3 rounded-md bg-[#F8EFD3] ring-2 ring-transparent border border-black/20 transition-all hover:shadow peer-checked:bg-[#F5DB8C]/75 peer-checked:ring-[#9D8443]/50 peer-checked:ring-offset-1 peer-checked:border-none">
-                                    <div className="flex flex-col gap-1">
-                                        <div className="items-center">
-                                        <p className="text-center lg:text-[16px] text-sm">Unable to attend</p>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </label>
-                            </div>
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <Label className="text-md" htmlFor="email">Number of guests</Label>
-                            <div className="w-full grid grid-cols-2 gap-4 text-center items-center">
-                                <label className="cursor-pointer">
-                                    <input
-                                    type="radio" className="peer sr-only"
-                                    name="guest"
-                                    value="1 Guest"
-                                    onChange={(e) => setGuest(e.target.value)}
-                                    />
-                                    <div className="lg:w-72 max-w-xl p-2 rounded-md bg-[#F8EFD3] ring-2 ring-transparent border border-black/20 transition-all hover:shadow peer-checked:bg-[#F5DB8C]/75 peer-checked:ring-[#9D8443]/50 peer-checked:ring-offset-1 peer-checked:border-none">
-                                    <div className="flex flex-col gap-1">
-                                        <div className="items-center">
-                                        <p className="text-center">1 Guest</p>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </label>
-                                <label className="cursor-pointer">
-                                    <input
-                                    type="radio" className="peer sr-only"
-                                    name="guest"
-                                    value="2 Guests"
-                                    onChange={(e) => setGuest(e.target.value)}
-                                    />
-                                    <div className="lg:w-72 max-w-xl p-2 rounded-md bg-[#F8EFD3] ring-2 ring-transparent border border-black/20 transition-all hover:shadow peer-checked:bg-[#F5DB8C]/75 peer-checked:ring-[#9D8443]/50 peer-checked:ring-offset-1 peer-checked:border-none">
-                                    <div className="flex flex-col gap-1">
-                                        <div className="items-center">
-                                        <p className="text-center">2 Guests</p>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </label>
-                            </div>
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <Label className="text-md" htmlFor="message">Message</Label>
-                            <Textarea
-                                className="min-h-[130px]"
-                                id="message"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                            />
-                        </div>
-                        <Toaster
-                        position="top-center"
-                        toastOptions={{ duration: 3000 }}
+    if (!name || !attendees || !message) {
+      toast.error("Please fill in all fields");
+      return;
+    }
 
-                        />
-                        <Button className="lg:mt-4 -mt-7 mb-1 bg-[#F5DB8C] hover:bg-[#f7d05c] border-[#FACB3D] text-black uppercase" type="submit">Submit</Button>
-                    </form>
-                </CardContent>
-            </Card>
-        </div>
-    );
+    setIsSubmitting(true); // start loading
+
+    try {
+      await fetch("/api/guestbook", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setIsCreated(true);
+      toast.success("Success");
+      // optionally reset form here
+      setName("");
+      setAttendees("");
+      setGuest("");
+      setMessage("");
+    } catch (error) {
+      console.error(error);
+      toast.error("Something went wrong");
+    } finally {
+      setIsSubmitting(false); // stop loading
+    }
+  };
+
+  return (
+    <div className="font-regular items-center mx-auto">
+      <Card className="flex flex-col md:w-150 lg:w-[700px] w-85 bg-[#F9F5EC] text-black lg:px-14 px-5 mx-auto lg:py-8 py-5">
+        <CardHeader>
+          <CardTitle className="lg:text-3xl text-xl uppercase text-[#9D8443] text-center">
+            attendance
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-7">
+            {/* nama */}
+            <div className="flex flex-col gap-1">
+              <Label className="text-md" htmlFor="name">
+                Guest Name
+              </Label>
+              <Input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            {/* attendance */}
+            <div className="flex flex-col gap-1">
+              <Label className="text-md" htmlFor="email">
+                Attendance
+              </Label>
+              <div className="w-full grid grid-cols-2 gap-2 text-center items-center">
+                <label className="cursor-pointer">
+                  <input
+                    type="radio"
+                    className="peer sr-only"
+                    id="attendees"
+                    name="attendees"
+                    value="attending"
+                    onChange={(e) => setAttendees(e.target.value)}
+                  />
+                  <div className="lg:w-72 max-w-xl p-2 lg:py-2 py-3 rounded-md bg-[#F8EFD3] ring-2 ring-transparent border border-black/20 transition-all hover:shadow peer-checked:bg-[#F5DB8C]/75 peer-checked:ring-[#9D8443]/50 peer-checked:ring-offset-1 peer-checked:border-none">
+                    <div className="flex flex-col gap-1">
+                      <p className="text-center lg:text-[16px] text-sm">
+                        Will attend
+                      </p>
+                    </div>
+                  </div>
+                </label>
+                <label className="cursor-pointer">
+                  <input
+                    type="radio"
+                    className="peer sr-only"
+                    name="attendees"
+                    value="unable to attend"
+                    onChange={(e) => setAttendees(e.target.value)}
+                  />
+                  <div className="lg:w-72 max-w-xl lg:py-2 py-3 rounded-md bg-[#F8EFD3] ring-2 ring-transparent border border-black/20 transition-all hover:shadow peer-checked:bg-[#F5DB8C]/75 peer-checked:ring-[#9D8443]/50 peer-checked:ring-offset-1 peer-checked:border-none">
+                    <div className="flex flex-col gap-1">
+                      <p className="text-center lg:text-[16px] text-sm">
+                        Unable to attend
+                      </p>
+                    </div>
+                  </div>
+                </label>
+              </div>
+            </div>
+            {/* guest */}
+            <div className="flex flex-col gap-1">
+              <Label className="text-md" htmlFor="email">
+                Number of guests
+              </Label>
+              <div className="w-full grid grid-cols-2 gap-4 text-center items-center">
+                <label className="cursor-pointer">
+                  <input
+                    type="radio"
+                    className="peer sr-only"
+                    name="guest"
+                    value="1 Guest"
+                    onChange={(e) => setGuest(e.target.value)}
+                  />
+                  <div className="lg:w-72 max-w-xl p-2 rounded-md bg-[#F8EFD3] ring-2 ring-transparent border border-black/20 transition-all hover:shadow peer-checked:bg-[#F5DB8C]/75 peer-checked:ring-[#9D8443]/50 peer-checked:ring-offset-1 peer-checked:border-none">
+                    <p className="text-center">1 Guest</p>
+                  </div>
+                </label>
+                <label className="cursor-pointer">
+                  <input
+                    type="radio"
+                    className="peer sr-only"
+                    name="guest"
+                    value="2 Guests"
+                    onChange={(e) => setGuest(e.target.value)}
+                  />
+                  <div className="lg:w-72 max-w-xl p-2 rounded-md bg-[#F8EFD3] ring-2 ring-transparent border border-black/20 transition-all hover:shadow peer-checked:bg-[#F5DB8C]/75 peer-checked:ring-[#9D8443]/50 peer-checked:ring-offset-1 peer-checked:border-none">
+                    <p className="text-center">2 Guests</p>
+                  </div>
+                </label>
+              </div>
+            </div>
+            {/* message */}
+            <div className="flex flex-col gap-1">
+              <Label className="text-md" htmlFor="message">
+                Message
+              </Label>
+              <Textarea
+                className="min-h-[130px]"
+                id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </div>
+            <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+            <Button
+              className="lg:mt-4 -mt-7 mb-1 bg-[#F5DB8C] hover:bg-[#f7d05c] border-[#FACB3D] text-black uppercase"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Loading..." : "Submit"}
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
-export default Guestbook
+export default Guestbook;
--- components/message.tsx
+++ components/message.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { Label } from "./ui/label";
-import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { useEffect, useState } from "react";
 
 interface GuestBook {
@@ -17,68 +23,95 @@ interface ApiResponse {
   status: number;
   data: GuestBook[];
 }
+
 function WeddingMessage() {
   const rowsPerPage = 4;
 
   const [startIndex, setStartIndex] = useState(0);
   const [endIndex, setEndIndex] = useState(rowsPerPage);
-
   const [data, setData] = useState<ApiResponse>({ status: 0, data: [] });
-  
-   useEffect(() => {
-    fetch('/api/message')
-      .then(response => response.json())
-      .then(data => {
-        setData(data)
-      });
-  }, [startIndex, endIndex]);
-  
+
+  const fetchMessages = async () => {
+    try {
+      const res = await fetch("/api/message");
+      const result = await res.json();
+      setData(result);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+useEffect(() => {
+  fetchMessages();
+  const interval = setInterval(fetchMessages, 5000);
+  return () => clearInterval(interval);
+}, []);
+
+
   const guestItems = data.data;
 
   return (
     <div>
       <div className="flex flex-col gap-2 items-center justify-center font-regular">
-            <Label className="lg:text-3xl text-xl lg:mb-8 mb-2 uppercase font-semibold text-[#3E3E3E]">Message </Label>
-            <div className="flex flex-col gap-5">
-              {Array.isArray(guestItems) && guestItems.slice(startIndex, endIndex).map((guestBook) => (
-                
-               <div
-               key={guestBook.id}
-               className="flex flex-col bg-black/50 text-white lg:w-[700px] w-80 lg:py-6 py-2 lg:px-8 px-8 rounded-lg  ">
-                    <Label className="lg:text-xl text-md font-semibold capitalize">{guestBook.name}</Label>
-                    <Label className="lg:text-md text-md mt-1 mb-1">{guestBook.createdAt.replace('T', '  -  ').slice(0, 20)}</Label>
-                    <p className="lg:text-xl text-md whitespace-pre-wrap">{guestBook.message.replace(/\\n/g, '\n')}</p>
-               </div>
+        <Label className="lg:text-3xl text-xl lg:mb-8 mb-2 uppercase font-semibold text-[#3E3E3E]">
+          Message
+        </Label>
+        <div className="flex flex-col gap-5">
+          {Array.isArray(guestItems) &&
+            guestItems
+              .slice(startIndex, endIndex)
+              .map((guestBook) => (
+                <div
+                  key={guestBook.id}
+                  className="flex flex-col bg-black/50 text-white lg:w-[700px] w-80 lg:py-6 py-2 lg:px-8 px-8 rounded-lg"
+                >
+                  <Label className="lg:text-xl text-md font-semibold capitalize">
+                    {guestBook.name}
+                  </Label>
+                  <Label className="lg:text-md text-md mt-1 mb-1">
+                    {guestBook.createdAt
+                      .replace("T", "  -  ")
+                      .slice(0, 20)}
+                  </Label>
+                  <p className="lg:text-xl text-md whitespace-pre-wrap">
+                    {guestBook.message.replace(/\\n/g, "\n")}
+                  </p>
+                </div>
               ))}
-            </div>
-          </div>
-        <Pagination >
-          <PaginationContent className="flex flex-row mt-8 lg:gap-138 gap-42 items-center">
-            <PaginationItem>
-              <PaginationPrevious
+        </div>
+      </div>
+      <Pagination>
+        <PaginationContent className="flex flex-row mt-8 gap-6 items-center">
+          <PaginationItem>
+            <PaginationPrevious
               className={
-                startIndex === 0 ? "pointer-events-none opacity-50" : undefined
+                startIndex === 0
+                  ? "pointer-events-none opacity-50"
+                  : undefined
               }
               onClick={() => {
                 setStartIndex(startIndex - rowsPerPage);
                 setEndIndex(endIndex - rowsPerPage);
-              }}/>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationNext 
+              }}
+            />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext
               className={
-                endIndex >= guestItems?.length ? "pointer-events-none opacity-50" : undefined
+                endIndex >= guestItems?.length
+                  ? "pointer-events-none opacity-50"
+                  : undefined
               }
               onClick={() => {
-                setStartIndex(startIndex + rowsPerPage); //10
-                setEndIndex(endIndex + rowsPerPage); //10 + 10 = 20
-              }} />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
+                setStartIndex(startIndex + rowsPerPage);
+                setEndIndex(endIndex + rowsPerPage);
+              }}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
-  
 }
 
 export default WeddingMessage;
